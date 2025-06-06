00c20668c9774d12ddfdd47114f2fecbfcb2e619
Welvin Salablanca <stivensalablanca@gmail.com>
2025-06-05 21:26:36 -0600

    Mensaje

--- src/Vista/VistaProducto.java
+++ src/Vista/VistaProducto.java
@@ -1,9 +1,11 @@
+package Vista;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
 
-package Vista;
+
 
 
 import Controlador.ProductoControlador;
